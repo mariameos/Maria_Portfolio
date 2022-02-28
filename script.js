7938b077e6065055c2248b485a1a64520399da30
mariameos <ms.meos@gmail.com>
2022-02-28 21:33:40 +0200

    adcampaigns lisamine

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-/*
-===============================================================
-
-Hi! Welcome to my little playground!
-
-My name is Tobias Bogliolo. 'Open source' by default and always 'responsive',
-I'm a publicist, visual designer and frontend developer based in Barcelona. 
-
-Here you will find some of my personal experiments. Sometimes usefull,
-sometimes simply for fun. You are free to use them for whatever you want 
-but I would appreciate an attribution from my work. I hope you enjoy it.
-
-===============================================================
-*/
-
+//kursor
 $(document).mousemove(function(e){
   //Get 'container' offset:
   o = $('.box').offset();
@@ -23,3 +9,15 @@ $(document).mousemove(function(e){
   });
 });
 
+//navbar
+$(function() {
+	$('nav.navabr a').bind('click',function(event){
+    event.preventDefault();
+		var $anchor = $(this);
+    console.log($anchor.attr('href'))
+		$('html, body').stop().animate({
+			scrollTop: $($anchor.attr('href')).offset().top
+		}, 1000);
+		event.preventDefault();
+	});
+});
